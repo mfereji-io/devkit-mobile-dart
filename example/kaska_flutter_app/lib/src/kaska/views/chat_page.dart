333eccec2daa0c1935810ebf430096a70669756f
zackmwangi <60732046+zackmwangi@users.noreply.github.com>
2023-04-27 08:59:25 +0300

    updating the chat page

--- example/kaska_flutter_app/lib/src/kaska/views/chat_page.dart
+++ example/kaska_flutter_app/lib/src/kaska/views/chat_page.dart
@@ -3,16 +3,23 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:kaskazini/util/service_locator.dart';
 
-import 'package:mfereji/mfereji.dart' as mfereji;
 import 'package:dash_chat_2/dash_chat_2.dart';
 
 final AuthenticationBloc authBloc = GetIt.instance<AuthenticationBloc>();
-final AppRouter appRouter = GetIt.instance<AppRouter>();
 
 class ChatPage extends StatefulWidget {
   ChatPage({super.key});
 
   final ChatClient cli = ChatClient();
+
+  String userId = authBloc.state.authenticatedUser.id;
+  String username = authBloc.state.authenticatedUser.username;
+  String firstname = authBloc.state.authenticatedUser.firstname;
+  String lastname = authBloc.state.authenticatedUser.lastname;
+
+  //Mfereji credentials
+  String chatToken = authBloc.state.authenticatedUser.chatToken;
+  String mferejiAppId = authBloc.state.authenticatedUser.chatAppId;
 
   @override
   State<ChatPage> createState() => _ChatPageState();
@@ -28,60 +35,37 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   void initState() {
-    if (authBloc.state is AuthenticationAuthenticated) {
-
-      String username = authBloc.state.authenticatedUser.username;
-      String chatToken = authBloc.state.authenticatedUser.chatToken;
 
       try {
 
         widget.cli
-        ..init(chatToken)
-        ..connect(() async {
-          //print("connected to mfereji presence server");
-        });
+        ..init(widget.chatToken)
+        ..connect(() async {});
 
       widget.cli.subscribe("kaskazinic");
 
-      
       _sub = widget.cli.messages.listen(
           (msg) => setState(
             () => messages.insert(0, msg)
           )
         );
       
-
       super.initState();
       isConnected = true;
 
       } catch (exception) {
-        print("could not connect to mfereji presence server: ${exception.toString()}");
-        
+        //print("could not connect to mfereji presence server: ${exception.toString()}");
       }
 
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<AuthenticationBloc, AuthenticationState>(
-        bloc: authBloc,
-        listener: (context, state) {
-          if (state is AuthenticationUnauthenticated) {
-            appRouter.replace(AuthLoginRoute());
-          }
-        },
-        builder: (context, state) {
             
-            String username = state.authenticatedUser.username;
-            String chatToken = state.authenticatedUser.chatToken;
-            String mferejiAppId = state.authenticatedUser.chatAppId;
-            //String mferejiAppId = "72303f7f-bcbb-428f-9314-21a6e2d7061c";
-
             ChatUser user = ChatUser(
-              id: state.authenticatedUser.id,
-              firstName: state.authenticatedUser.firstname,
-              lastName: state.authenticatedUser.lastname,
+              id: widget.userId,
+              firstName: widget.firstname,
+              lastName: widget.lastname,
             );
 
             if (isConnected) {
@@ -92,24 +76,18 @@ class _ChatPageState extends State<ChatPage> {
                 child: DashChat(
                   currentUser: user,
                   onSend: (ChatMessage m) {
-                    //push to mfereji chat-api
                     widget.cli.sendMsg(m);
-                    /*
-                    setState(() {
-                      //messages.insert(0, m);
-                    });
-                    */
                   },
                   messages: messages,
                 ),
               );
             } else {
               return Container(
-                child: Text("Currently disconnected"),
+                child: Text("Currently disconnected from mfereji chat service"),
               );
             }
 
-        });
+        
   }
 
   @override
